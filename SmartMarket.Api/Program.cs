e81e2d35991ff592bd221825cbd8c22f0e40d4fa
dotnetbroo <jasurbekabdunazarov04@gmail.com>
2024-10-22 09:18:28 +0500

    DGDG

--- SmartMarket.Api/Program.cs
+++ SmartMarket.Api/Program.cs
@@ -78,7 +78,7 @@ public class Program
         app.UseAuthentication();
         app.UseAuthorization();
 
-        app.Urls.Add("http://127.0.0.1:9999");
+        app.Urls.Add("http://127.0.0.1:25325");
         app.MapControllers();
 
         app.Run();
